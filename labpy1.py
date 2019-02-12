96c2d47e5237e8ff35b2a81fee2e8264e148eeed
aditiaseptiana <45659195+aditiaseptiana@users.noreply.github.com>
2019-02-12 06:43:34 -0800

    Add files via upload

---
+++ labpy1.py
@@ -0,0 +1,8 @@
+a = int(input("Masukan Bilangan Pertama ="))
+b = int(input("Masukan Bilangan Kedua ="))
+c = int(input("Masukan Bilangan Ke Tiga = "))
+if a>b and a>c :
+    print("Bilangan Terbesar Adalah =", a)
+elif b>a and b>c :
+    print("Bilangan Terbesar Adalah=",b)
+else : print("Bilangan Terbesar Adalah",c)
